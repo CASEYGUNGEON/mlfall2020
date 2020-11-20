effc03e858efa1d7acb906dbcb5d9409e4d1d762
BrandonCerezo <64775527+BrandonCerezo@users.noreply.github.com>
2020-11-19 19:48:52 -0800

    added testing results to linear regression model

--- regression.py
+++ regression.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 # from download_data import download_data # Not using this
 # from GD import gradientDescent
 # from dataNormalization import rescaleMatrix
@@ -18,22 +19,21 @@ arr = pd.DataFrame(data).to_numpy()
 #########################################################################
 # Normalize Data
 
-# get min
-minval = np.zeros(13)
-dif = np.zeros(13)
-for i in range(0,12):
-    minval[i] = arr[:,i].min()
-    dif[i] = arr[:,i].max() - minval[i]
-    for j in range(0,4898):
-        arr[j,i] = (arr[j,i] - minval[i]) / dif[i]
+minval = np.zeros(13) 
+dif = np.zeros(13)    
+for i in range(0,12): # for each category
+    minval[i] = arr[:,i].min() # get min
+    dif[i] = arr[:,i].max() - minval[i] # get difference
+    for j in range(0,4898): # for each value in each category
+        arr[j,i] = (arr[j,i] - minval[i]) / dif[i] # normalizing data
 
-print(arr)
+# print(arr) # for testing
 
 # Assign values for X and Y
 Xvals = np.ones((4898,12))
 Yvals = np.zeros(4898)
-Xvals[:, 1:12] = arr[:, 0:11]
-Yvals[:] = arr[:, 11]
+Xvals[:, 1:12] = arr[:, 0:11] # Xvals[:, 0] is 1, fill in the rest with the X values from the dataset
+Yvals[:] = arr[:, 11] # fill in Y values from dataset
 
 #print(Xvals)
 #print(Yvals)
@@ -41,7 +41,7 @@ Yvals[:] = arr[:, 11]
 #########################################################################
 # Number of training samples
 # Can be changed
-TrainNum = 2000
+TrainNum = 2000 
 
 # Splitting data into testing and training 
 Xtrain = np.zeros((TrainNum,12))
@@ -71,9 +71,9 @@ m = len(Ytrain)
 arrCost = [];
 XT = np.transpose(Xtrain)
 theta = np.zeros(12)
-[a,b] = XT.shape
-print(a)
-print(b)
+#[a,b] = XT.shape
+#print(a)
+#print(b)
 # Finding Theta
 for iterations in range(0, numIterations):
     sum = np.zeros(12)
@@ -83,7 +83,7 @@ for iterations in range(0, numIterations):
         for j in range(0,12):
             sum[j] = (dot - Ytrain[i]) * XT[j,i] + sum[j]
             #print(XT[j,i])
-    sum[:] = alpha/(m+1) * sum[:]
+    sum[:] = alpha/(m) * sum[:]
     theta = np.subtract(theta, sum)
 
 # Getting Cost Function 
@@ -91,15 +91,48 @@ for iterations in range(0, numIterations):
     sum1 = 0
     for i in range(0,m):
         sum1 = (theta.dot(XT[:,i]) - Ytrain[i])**2 + sum1
-    atmp = sum1/2/(m+1)
+    atmp = sum1/2/(m)
     #print(atmp)
     arrCost.append(atmp)
 print("Done training")
 
 ##########################################################################
-# Plotting results of cost function
+# Test how well the predictions and ground-truth values match
+print("Start testing")
+testNum = len(Ytest)
+print(testNum)
 
-plt. plot(range(0,len(arrCost)),arrCost);
+predictions = np.zeros(testNum)
+Error = np.zeros(testNum)
+TransposedTesting = np.transpose(Xtest)
+#print(np.shape(TransposedTesting))
+L1Err = L2Err = STDErr = mean = 0
+for i in range(0,testNum):
+    predictions[i] = theta.dot(TransposedTesting[:,i])
+    L1Err = abs(predictions[i] - float(Ytest[i])) + L1Err # uses absolute value
+    L2Err = (predictions[i] - float(Ytest[i]))**2 + L2Err # uses square of error
+    Error[i] = predictions[i] - float(Ytest[i]) # for calculating standard deviation
+    mean = Error[i] + mean
+   #print(i)
+
+mean = mean/testNum
+for i in range(0,testNum):
+    STDErr = abs(Error[i] - mean)**2 + STDErr
+
+STDErr = math.sqrt(STDErr / testNum)
+L1Error = L1Err/testNum
+L2Error = L2Err/testNum
+print(L1Error)
+print(L2Error)
+print(STDErr)
+print("Done testing")
+
+##########################################################################
+# Plotting results of cost function from training
+# NOTE: all plots must be added to the end of the program, otherwise any code
+#       afterwards may not run
+
+plt.plot(range(0,len(arrCost)),arrCost);
 plt.xlabel('iteration')
 plt.ylabel('cost')
 plt.title('alpha = {} theta = {}'.format(alpha, theta))
