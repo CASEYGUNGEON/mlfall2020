193a1c2116daf8b5c0eda1b4cbeff4d8c4d9c8ca
casey k9 <connor@campi.cc>
2020-11-20 12:39:55 -0800

    added distance function to KNN

--- knn.py
+++ knn.py
@@ -5,7 +5,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
+# calculate distance between 2 points
+def distance(x1, x2):
+    dist = 0.0
+    for i in range(len(x1)-1):
+        dist += (x1[i] - x2[i])**2
+        dist = sqrt(dist)
+    return dist
+
 #read data from csv
 
 data = pd.read_csv('.\\winequality-white.csv', sep = ';')
 arr = pd.DataFrame(data).to_numpy()
+
