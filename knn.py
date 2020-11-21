8d7a8e2227e82355a94a37a0ccf2f3f85dd1fc43
casey k9 <connor@campi.cc>
2020-11-21 14:39:18 -0800

    KNN: added debug code to sample picking

--- knn.py
+++ knn.py
@@ -17,14 +17,16 @@ def distance(x1, x2):
 
 data = pd.read_csv('.\\winequality-white.csv', sep = ';')
 arr = pd.DataFrame(data).to_numpy()
-
+print(len(arr))
 # normalize
 minVal = np.zeros(12)
 maxDif = np.zeros(12)
-for i in range(0,12): #get minimum and range for each factor & result
+    # get minimum and range for each factor & result
+for i in range(0,12):
     minVal[i] = arr[:,i].min()
     maxDif[i] = arr[:,i].max() - minVal[i]
-    for x in range(0,len(arr)): # perform normalization on each factor in each datum
+        # perform normalization on each factor in each datum
+    for x in range(0,len(arr)):
         arr[x,i] = (arr[x,i] - minVal[i]) / maxDif[i]
 
 
@@ -36,20 +38,32 @@ Y = arr[:,11]
 
 # pick random sample for training
 numTrainSamples = 2000
-trainPicks = np.randInt(0,len(arr),numTrainSamples)
+trainPicks = np.random.randint(0,len(arr),numTrainSamples)
 
-# separate training samples
-XTrain = np.zeros((numTrainSamples,12))
+# initialize arrays for training and test sets
+XTrain = np.zeros((numTrainSamples,10))
 YTrain = np.zeros(numTrainSamples)
-XTest = np.zeros((len(arr) - numTrainSamples,12))
+XTest = np.zeros((len(arr) - numTrainSamples,10))
 YTest = np.zeros(len(arr) - numTrainSamples)
+
+print(len(XTest))
+
+# separate training and test samples
 curTrainIndex = 0
 curTestIndex = 0
-for i in arr:
+for i in range(0,len(arr) - 1):
     if i in trainPicks:
+        print("add to train: " + str(curTestIndex) + " + " + str(i))
         XTrain[curTrainIndex] = X[i]
         YTrain[curTrainIndex] = Y[i]
         curTrainIndex += 1
     else:
+        print("add to test: " + str(curTestIndex) + " + " + str(i))
         XTest[curTestIndex] = X[i]
         YTest[curTestIndex] = Y[i]
+        curTestIndex += 1
+
+#print(XTrain)
+#print(YTrain)
+#print(XTest)
+#print(YTest)
