6b1452f4364c6663e49117d440e71ba8138d9274
casey k9 <connor@campi.cc>
2020-11-21 00:46:30 -0800

    normalize data for KNN

--- knn.py
+++ knn.py
@@ -10,11 +10,18 @@ def distance(x1, x2):
     dist = 0.0
     for i in range(len(x1)-1):
         dist += (x1[i] - x2[i])**2
-        dist = sqrt(dist)
+    dist = sqrt(dist)
     return dist
 
-#read data from csv
+# read data from csv
 
 data = pd.read_csv('.\\winequality-white.csv', sep = ';')
 arr = pd.DataFrame(data).to_numpy()
+
+# normalize
+for i in range(0,13): #get minimum and range for each factor & result
+    minval[i] = arr[:,i].min()
+    maxdif[i] = arr[:,i].max - minval[i]
+    for x in arr.len(): # perform normalization on each factor in each datum
+        arr[x,i] = (arr[x,i] - minval[i]) / maxdif[i]
 
