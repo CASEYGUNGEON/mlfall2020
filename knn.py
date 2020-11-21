022769510f64b58b02d51746062b1085a003a151
casey k9 <connor@campi.cc>
2020-11-21 14:01:45 -0800

    KNN: create training and test sample sets

--- knn.py
+++ knn.py
@@ -19,15 +19,37 @@ data = pd.read_csv('.\\winequality-white.csv', sep = ';')
 arr = pd.DataFrame(data).to_numpy()
 
 # normalize
-minval = np.zeros(12)
-maxdif = np.zeros(12)
+minVal = np.zeros(12)
+maxDif = np.zeros(12)
 for i in range(0,12): #get minimum and range for each factor & result
-    minval[i] = arr[:,i].min()
-    maxdif[i] = arr[:,i].max() - minval[i]
+    minVal[i] = arr[:,i].min()
+    maxDif[i] = arr[:,i].max() - minVal[i]
     for x in range(0,len(arr)): # perform normalization on each factor in each datum
-        arr[x,i] = (arr[x,i] - minval[i]) / maxdif[i]
+        arr[x,i] = (arr[x,i] - minVal[i]) / maxDif[i]
 
+
+# separate X and Y values for easier use
 X = np.zeros((len(arr),11))
 Y = np.zeros(len(arr))
 X = arr[:,0:10]
 Y = arr[:,11]
+
+# pick random sample for training
+numTrainSamples = 2000
+trainPicks = np.randInt(0,len(arr),numTrainSamples)
+
+# separate training samples
+XTrain = np.zeros((numTrainSamples,12))
+YTrain = np.zeros(numTrainSamples)
+XTest = np.zeros((len(arr) - numTrainSamples,12))
+YTest = np.zeros(len(arr) - numTrainSamples)
+curTrainIndex = 0
+curTestIndex = 0
+for i in arr:
+    if i in trainPicks:
+        XTrain[curTrainIndex] = X[i]
+        YTrain[curTrainIndex] = Y[i]
+        curTrainIndex += 1
+    else:
+        XTest[curTestIndex] = X[i]
+        YTest[curTestIndex] = Y[i]
